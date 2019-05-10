7dbf4876b0c518a2db643ace8d8fb6ceaf5d20f2
connor cahill <connorcahill913@gmail.com>
2019-05-10 23:15:39 +0000

    [Fixes]: install docker-compose command.

--- fresh-docker-droplet.sh
+++ fresh-docker-droplet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 curl -fsSL https://get.docker.com -o get-docker.sh \
-    && sh get-docker.sh
-apt intsall -y docker-compose
+    && sh get-docker.sh \
+    && apt intsall docker-compose
+if (($?)); then apt-get update && apt-get install -y docker-compose;fi
